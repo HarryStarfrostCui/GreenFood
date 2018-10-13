c6634854fac40ade04e159a4397f29a0a7395143
Andy <xca75@sfu.ca>
2018-10-12 20:45:57 -0700

    Empty Android Activity

---
+++ app/src/main/java/xca75/sfu/ca/greenfood/MainActivity.java
@@ -0,0 +1,13 @@
+package xca75.sfu.ca.greenfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
