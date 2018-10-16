9e96eb8237463813116a247ad330446083fe84b1
Andy <xca75@sfu.ca>
2018-10-16 00:41:42 -0700

    put AddingFoodPage into the new branch

---
+++ app/src/main/java/com/example/hca127/greenfood/AddingFoodPage.java
@@ -0,0 +1,13 @@
+package com.example.hca127.greenfood;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddingFoodPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding_food_page);
+    }
+}
