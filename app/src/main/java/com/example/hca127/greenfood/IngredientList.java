0e8bcad7047d1608d6d731fe3e0017b1eb318723
hca127 <harry_cui@sfu.ca>
2018-10-17 11:52:18 -0700

    basic data class + basic unit test

--- app/src/main/java/com/example/hca127/greenfood/IngredientList.java
+++ app/src/main/java/com/example/hca127/greenfood/IngredientList.java
@@ -8,38 +8,55 @@ public class IngredientList {
     private ArrayList<Integer> amount;
     private ArrayList<Double> carbonEmit;
 
-    public IngredientList() {
+    IngredientList() {
         name = new ArrayList<String>();
         carbon_coefficient = new ArrayList<Double>();
         amount = new ArrayList<Integer>();
         carbonEmit = new ArrayList<Double>();
+        /*
+        ArrayList<String> food = new ArrayList<>(
+                Arrays.asList("Lamb", "Beef", "Cheese", "Pork", "Salmon",
+                        "Turkey", "Chicken", "Tuna", "Eggs", "Potatoes",
+                        "Rice", "Peanut Butter", "Nuts", "Yogurt", "Broccoli",
+                        "Tofu", "Dry Beans", "Milk(2%)", "Tomatoes", "Lentils")
+        );
+        ArrayList<Double> carbon = new ArrayList<>(
+                Arrays.asList(39.2, 12.0, 13.5, 12.1, 11.9,
+                        10.9, 6.9, 6.1, 4.8, 2.9,
+                        2.7, 2.5, 2.3, 2.2, 2.0,
+                        2.0, 2.0, 1.9, 1.1, 0.9)
+        );
+        for (int i = 0; i < food.size(); i++){
+            addIng(food.get(i), carbon.get(i));
+        }
+        */
 
     }
 
-    public String getName(int index) {
+    String getName(int index) {
         return name.get(index);
     }
 
-    public double getCarbon(int index) {
+    double getCarbon(int index) {
         return carbon_coefficient.get(index);
     }
 
-    public int getAmout(int index) {
+    int getAmout(int index) {
         return amount.get(index);
     }
 
-    public int getsize() {
+    int getsize() {
         return name.size();
     }
 
-    public void addIng(String food_name, double Carbon) {
+    void addIng(String food_name, double Carbon) {
         name.add(food_name);
         carbon_coefficient.add(Carbon);
         amount.add(0);
         carbonEmit.add(0.0);
     }
 
-    public boolean setIngAmount(int index, int Amount) {
+    boolean setIngAmount(int index, int Amount) {
         if (index >= getsize()) {
             return false;
         }
@@ -48,11 +65,12 @@ public class IngredientList {
         return true;
     }
 
-    public double getTotalEmit() {
+    double getTotalEmit() {
         int i;
         double sum = 0;
-        for (i = 1; i < carbonEmit.size(); i++)
+        for (i = 0; i < carbonEmit.size(); i++) {
             sum += carbonEmit.get(i);
+        }
         return sum;
     }
 
