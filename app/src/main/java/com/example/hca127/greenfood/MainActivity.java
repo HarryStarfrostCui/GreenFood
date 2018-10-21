83ba9befc5864bf5a40c9a7cbc31d053e3bfef75
hca127 <harry_cui@sfu.ca>
2018-10-21 14:03:43 -0700

    Revert "remove EVERY DAMN THING"
    This reverts commit 0642d1a19fb1df0da713aa3133ad42d11788ad4c

---
+++ app/src/main/java/com/example/hca127/greenfood/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.hca127.greenfood;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button weekly, meal, about;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        weekly = findViewById(R.id.weekly);
+        weekly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddingFoodPage.class);
+
+                //intent.putExtra(x,y);
+                startActivity(intent);
+            }
+        });
+
+        meal = findViewById(R.id.meal);
+        meal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), meal_calc.class);
+                //intent.putExtra(x,y);
+                startActivity(intent);
+            }
+        });
+
+        about = findViewById(R.id.about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), about_page.class);
+                //intent.putExtra(x,y);
+                startActivity(intent);
+            }
+        });
+    }
+}
