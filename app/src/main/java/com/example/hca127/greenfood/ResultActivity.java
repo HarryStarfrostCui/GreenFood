e1ef1b492886d2f0a47d28b92d7d21f55b4a28af
ndolf <ndolf@sfu.ca>
2018-10-24 00:41:20 -0700

    Remove Activity Titles except on Main Activity Small code and string clean up

--- app/src/main/java/com/example/hca127/greenfood/ResultActivity.java
+++ app/src/main/java/com/example/hca127/greenfood/ResultActivity.java
@@ -16,13 +16,13 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        Bundle extras = getIntent().getExtras();
-        basket = extras.getIntegerArrayList("basket");
-        Toast.makeText(ResultActivity.this,
-                "its " +   basket.get(0).toString() + " "  + basket.get(1).toString() + " " + basket.get(2).toString() + " "  +
-                                basket.get(3).toString() + " "  + basket.get(4).toString() + " " + basket.get(5).toString() + " "  +
-                                basket.get(6).toString() + " "  + basket.get(7).toString(),
-                Toast.LENGTH_LONG).show();
+//        Bundle extras = getIntent().getExtras();
+//        basket = extras.getIntegerArrayList("basket");
+//        Toast.makeText(ResultActivity.this,
+//                "its " +   basket.get(0).toString() + " "  + basket.get(1).toString() + " " + basket.get(2).toString() + " "  +
+//                                basket.get(3).toString() + " "  + basket.get(4).toString() + " " + basket.get(5).toString() + " "  +
+//                                basket.get(6).toString() + " "  + basket.get(7).toString(),
+//                Toast.LENGTH_LONG).show();
 
     }
 }
