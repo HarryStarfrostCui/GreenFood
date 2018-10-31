ffcc9b7b8a2257bffa3aec97ac54588e8633de6e
hca127 <harry_cui@sfu.ca>
2018-10-30 17:01:21 -0700

    slight update of UI of menu bar sampled in main

--- app/src/main/java/com/example/hca127/greenfood/MainActivity.java
+++ app/src/main/java/com/example/hca127/greenfood/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.hca127.greenfood;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+
+    //  https://www.youtube.com/watch?v=zYVEMCiDcmY  (done)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawer = findViewById(R.id.drawer);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.Navigation_drawer_open, R.string.Navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
