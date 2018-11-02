a8e3086b12c350baa9a6adcfa9c702fa894d8982
Andy <xca75@sfu.ca>
2018-11-01 22:05:58 -0700

    a bunch of layouts

--- app/src/main/java/com/example/hca127/greenfood/LoginActivity.java
+++ app/src/main/java/com/example/hca127/greenfood/LoginActivity.java
@@ -1,12 +1,48 @@
 package com.example.hca127.greenfood;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText email_input = (EditText) findViewById(R.id.email_input);
+        email_input.requestFocus();
+
+
+        signUp = (TextView) findViewById(R.id.sign_up_button);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    /*public void signUp(View view) {
+        signUp = (TextView) findViewById(R.id.sign_up_button);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }*/
 }
