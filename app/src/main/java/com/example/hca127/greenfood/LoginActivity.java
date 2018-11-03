e3b37318f33f015a3e456e97c4f8bea0bec8bf08
Andy <xca75@sfu.ca>
2018-11-03 11:28:33 -0700

    Google sign in, more layout stuff

--- app/src/main/java/com/example/hca127/greenfood/LoginActivity.java
+++ app/src/main/java/com/example/hca127/greenfood/LoginActivity.java
@@ -3,16 +3,30 @@ package com.example.hca127.greenfood;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageView signUp;
     private ImageView logoCommunity;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions google_sign_in_options;
+    private SignInButton google_sign_in_button;
+    public Integer RC_SIGN_IN = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +35,6 @@ public class LoginActivity extends AppCompatActivity {
 
         EditText email_input = (EditText) findViewById(R.id.email_input);
         email_input.requestFocus();
-
 
         signUp = (ImageView) findViewById(R.id.sign_up_button);
         signUp.setOnClickListener(new View.OnClickListener() {
@@ -41,10 +54,34 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        google_sign_in_button = (SignInButton) findViewById(R.id.google_sign_in_button);
+        google_sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent,RC_SIGN_IN);
+            }
+        });
 
-
-
+        google_sign_in_options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, google_sign_in_options);
     }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            findViewById(R.id.google_sign_in_button).setVisibility(View.GONE);
+            Intent communityPage = new Intent(LoginActivity.this, CommunityActivity.class);
+            startActivity(communityPage);
+        }
+    }
+
 
     /*public void signUp(View view) {
         signUp = (TextView) findViewById(R.id.sign_up_button);
