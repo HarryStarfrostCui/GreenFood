d1d0babb531b7adb9d491b4caa1c02d854a28b89
xca75 <andy_chen_15@sfu.ca>
2018-11-05 01:29:24 -0800

    Revert "Merge branch 'login_layout' into 'master'"
    This reverts merge request !52

--- app/src/main/java/com/example/hca127/greenfood/CommunityActivity.java
+++ app/src/main/java/com/example/hca127/greenfood/CommunityActivity.java
@@ -7,15 +7,11 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class CommunityActivity extends AppCompatActivity {
 
     private ImageView about_button;
-    private ImageView pledge_button;
-    private TextView temp;
     private String name;
-    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +21,16 @@ public class CommunityActivity extends AppCompatActivity {
         /*Bundle google_account_info = getIntent().getExtras();
         name = google_account_info.get("name").toString();*/
 
-        SharedPreferences google_account_info = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        name = google_account_info.getString("temp","");
-        email = google_account_info.getString("email","");
-
-        temp = findViewById(R.id.number_of_participants);
-        temp.setText(name);
+        SharedPreferences google_account_info = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = google_account_info.getString("name","");
+        String email = google_account_info.getString("email","");
 
 
-        about_button = (ImageView) findViewById(R.id.about_button_in_community_activity);
+        about_button = (ImageView) findViewById(R.id.about_button_community);
         about_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CommunityActivity.this, AboutActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        pledge_button = (ImageView) findViewById(R.id.pledge_button);
-        pledge_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CommunityActivity.this, PledgeActivity.class);
                 startActivity(intent);
             }
         });
