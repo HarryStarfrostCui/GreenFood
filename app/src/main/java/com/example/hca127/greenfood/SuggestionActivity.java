e49c26c4b12f63569fdd8e1d467a55a985b2dd70
wuzhenw <wuzhenw@sfu.ca>
2018-11-07 21:18:20 -0800

    try to figure out how to share photo but not working when everything is fine and no error shows up

--- app/src/main/java/com/example/hca127/greenfood/SuggestionActivity.java
+++ app/src/main/java/com/example/hca127/greenfood/SuggestionActivity.java
@@ -2,11 +2,15 @@ package com.example.hca127.greenfood;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.camera2.TotalCaptureResult;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,8 +21,12 @@ import com.example.hca127.greenfood.objects.Diet;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.share.ShareApi;
 import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareHashtag;
 import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareDialog;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
@@ -27,6 +35,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.gson.Gson;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class SuggestionActivity extends AppCompatActivity {
@@ -77,7 +86,7 @@ public class SuggestionActivity extends AppCompatActivity {
         BarDataSet barDataSet = new BarDataSet(entries, "BarDataSet");
         barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
 
-        BarData suggestionData = new BarData(barDataSet);
+        final BarData suggestionData = new BarData(barDataSet);
         mSuggestionChart.getXAxis().setDrawGridLines(false);
         mSuggestionChart.getLegend().setEnabled(false);
         mSuggestionChart.getAxisRight().setAxisMinimum(0f);
@@ -88,11 +97,10 @@ public class SuggestionActivity extends AppCompatActivity {
         mSuggestionChart.animateY(1200);
         mSuggestionChart.invalidate();
 
-        float carbonSaved = mDiet.getSuggestedDietSavingAmount() *.9f * 2463000f / 1000;
-        float treesSaved = carbonSaved/22;  // carbon offset of trees
+        float carbonSaved = mDiet.getSuggestedDietSavingAmount() * .9f * 2463000f / 1000;
+        float treesSaved = carbonSaved / 22;  // carbon offset of trees
 
-        if(carbonSaved < 0)
-        {
+        if (carbonSaved < 0) {
             carbonSaved = 0;
             treesSaved = 0;
         }
@@ -110,32 +118,63 @@ public class SuggestionActivity extends AppCompatActivity {
                 shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
                     @Override
                     public void onSuccess(Sharer.Result result) {
-                        Toast.makeText(SuggestionActivity.this,"share success!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SuggestionActivity.this, "share success!", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onCancel() {
-                        Toast.makeText(SuggestionActivity.this,"cancel", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SuggestionActivity.this, "cancel", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onError(FacebookException error) {
-                        Toast.makeText(SuggestionActivity.this,error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SuggestionActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
-                ShareLinkContent linkContent = new ShareLinkContent.Builder()
-                        .setContentUrl(Uri.parse("https://cmpt276.sfu.rosielab.ca/project"))
-                        .setQuote("testing")
+//                ShareLinkContent linkContent = new ShareLinkContent.Builder()
+//                        .setContentUrl(Uri.parse("https://cmpt276.sfu.rosielab.ca/project"))
+//                        .setQuote("testing")
+//                        .build();
+//                if (ShareDialog.canShow(ShareLinkContent.class))
+//                {
+//                    shareDialog.show(linkContent);
+//                }
+
+
+                Bitmap mLogoImage = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.logo);
+                SharePhoto mSharePhoto = new SharePhoto.Builder()
+                        .setBitmap(mLogoImage)
                         .build();
-                if (ShareDialog.canShow(ShareLinkContent.class))
+                SharePhotoContent content = new SharePhotoContent.Builder()
+                        .addPhoto(mSharePhoto)
+                        .setShareHashtag(new ShareHashtag.Builder().setHashtag("#I dialog").build())
+                        .build();
+                ShareApi.share(content, null);
+                if (ShareDialog.canShow(SharePhotoContent.class))
                 {
-                    shareDialog.show(linkContent);
+                    shareDialog.show(content);
                 }
             }
         });
 
+//        mFacebookShare.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent shareIntent = new Intent();
+//                shareIntent.setAction(Intent.ACTION_SEND);
+//                shareIntent.putExtra(Intent.EXTRA_TEXT, Uri.parse("https://cmpt276.sfu.rosielab.ca/project"));
+//                shareIntent.setType("text/plain");
+//                startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.welcome)));
+//            }
+//        });
+
     }
 
+    private String getURLForResource(int resourceID) {
+        return Uri.parse("android.resource://"+R.class.getPackage().getName()+"/"+resourceID).toString();
+    }
+
+    //}
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
