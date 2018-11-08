17c76fba62ca7520fee1319a9d408d75024980a4
hca127 <harry_cui@sfu.ca>
2018-11-08 13:34:22 -0800

    fixed about button in community fragment

--- app/src/main/java/com/example/hca127/greenfood/fragments/CommunityFragment.java
+++ app/src/main/java/com/example/hca127/greenfood/fragments/CommunityFragment.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.hca127.greenfood.R;
 
@@ -25,6 +27,16 @@ public class CommunityFragment extends Fragment {
         String name = google_account_info.getString("name","");
         String email = google_account_info.getString("email","");
 
+        ImageView aboutPage = (ImageView) view.findViewById(R.id.about_button_community);
+        aboutPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutFragment()).addToBackStack(null).commit();
+                NavigationView navigationView = getActivity().findViewById(R.id.navigation_view);
+                navigationView.setCheckedItem(R.id.menu_about);
+            }
+        });
         return view;
     }
 }
