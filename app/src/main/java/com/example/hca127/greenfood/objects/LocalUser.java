17dbcb1bed5a83e935496294ccfb8933f858fb8a
oandalib <omid_andalib@sfu.ca>
2018-11-08 18:13:12 -0800

    Update LocalUser.java

--- app/src/main/java/com/example/hca127/greenfood/objects/LocalUser.java
+++ app/src/main/java/com/example/hca127/greenfood/objects/LocalUser.java
@@ -20,7 +20,8 @@ public class LocalUser implements Serializable {
         mUserEmail = "anony@moose.com";
         mPledge = 0.0;
         mCityIndex = 0;
-        mDietList = new ArrayList<>();
+        mDietList = new ArrayList<Diet>();
+        mDietList.add(new Diet(true));
     }
 
     public String getUserId() {        return mUserId;    }
