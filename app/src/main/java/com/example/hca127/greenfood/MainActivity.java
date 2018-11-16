275e28b38f70e050655d5db0e724ae50fcdbbfb4
hca127 <harry_cui@sfu.ca>
2018-11-15 19:48:17 -0800

    login & start up load now functioning

--- app/src/main/java/com/example/hca127/greenfood/MainActivity.java
+++ app/src/main/java/com/example/hca127/greenfood/MainActivity.java
@@ -92,12 +92,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
                     mLocalUser.setCity((int)(long) dataSnapshot.child("city").getValue());
                     mLocalUser.setProfileIcon((int)(long)dataSnapshot.child("icon_index").getValue());
-                    int n = (int)(long)dataSnapshot.child("emission").child("NofEmission").getValue();
+                    int n = (int)(long)dataSnapshot.child("emissions").child("NofEmission").getValue();
                     ArrayList<Emission> nEmission = new ArrayList<>();
                     for(int i = 0; i<n; i++){
                         String tempDate = (String) dataSnapshot.child("emission")
                                 .child(String.valueOf(i)).child("date").getValue();
-                        double tempAmount = (double) dataSnapshot.child("emission")
+                        double tempAmount = (double) dataSnapshot.child("emissions")
                                 .child(String.valueOf(i)).child("amount").getValue();
                         nEmission.add(new Emission(tempDate,tempAmount));
                     }
