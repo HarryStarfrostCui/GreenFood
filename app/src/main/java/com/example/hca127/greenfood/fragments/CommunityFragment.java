7066f9eda902bc503a357d2b7e7a595ae2629cf7
ndolf <ndolf@sfu.ca>
2018-11-15 16:48:46 -0800

    Filter pledge by city in Community

--- app/src/main/java/com/example/hca127/greenfood/fragments/CommunityFragment.java
+++ app/src/main/java/com/example/hca127/greenfood/fragments/CommunityFragment.java
@@ -24,6 +24,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -88,11 +89,16 @@ public class CommunityFragment extends Fragment implements AdapterView.OnItemSel
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         mProgressBar.setVisibility(View.VISIBLE);
+        Query userPledges;
+
         if(position==0){
             mCurrentCity = "total";
+            userPledges= mDatabase.child("users").orderByChild("city");
         }else {
             mCurrentCity = Integer.toString(position);
+            userPledges= mDatabase.child("users").orderByChild("city").equalTo(position);
         }
+
         DatabaseReference community = mDatabase.child("Community pledge").child(mCurrentCity);
         community.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -116,12 +122,11 @@ public class CommunityFragment extends Fragment implements AdapterView.OnItemSel
             }
         });
 
-        final DatabaseReference userPledges = mDatabase.child("users");
         userPledges.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Iterable<DataSnapshot> userChildren = dataSnapshot.getChildren();
-
+                clearOtherPledges();
                 int index = 0;
                 for (DataSnapshot user : userChildren) {
                     String temp = String.format(getResources().getString(R.string.community_other_pledge),
@@ -145,5 +150,12 @@ public class CommunityFragment extends Fragment implements AdapterView.OnItemSel
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+    }
+
+    private void clearOtherPledges() {
+        for (int i = 0; i < mOtherPledges.size(); i++) {
+            mOtherPledges.get(i).setText("");
+        }
+
     }
 }
